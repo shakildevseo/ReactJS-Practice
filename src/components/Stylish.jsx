f83ae94d5a164dc5e38c8bbfc42204b408178cf5
shakildevseoo <shakildevseoo@gmail.com>
2025-12-22 09:21:45 +0600

    external css practice

--- src/components/Stylish.jsx
+++ src/components/Stylish.jsx
@@ -1,8 +1,27 @@
+import "./externalCss.css"
 
 function Stylish(){
     return(
         <>
+        {/* Inline CSS Start */}
         <h1  style={{backgroundColor : "black", color : "white", textAlign : "center", margin: "10px 0"}}>Hey This style from Inline CSS</h1>
+         {/* Inline CSS End */}
+
+
+
+
+          {/* External  CSS Start */}
+
+          <h2 className="externalCss">Hey This Style From External File(External CSS)</h2>
+
+           {/* External  CSS End  */}
+
+
+
+
+
+
+          
         
         </>
     )
