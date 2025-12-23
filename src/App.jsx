21bb1a98abe18122b07ce92ba939e557f8552c8b
shakildevseoo <shakildevseoo@gmail.com>
2025-12-24 02:55:47 +0600

    props-distructuring

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 import CardContainer from "./components/CardComponent/CardContainer";
+import Demo from "./components/CardComponent/Demo/Demo";
 import DateAndTime from "./components/DateAndTime";
+
 
 
 function App() {
   return (
     <>
    {/* <CardContainer /> */}
-   <DateAndTime />
+   {/* <DateAndTime /> */}
+
+   <Demo  name = "Md Shakil Hossain" desc= "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos soluta id perspiciatis magni voluptas veniam! Laboriosam iure commodi assumenda temporibus doloremque, ad minus quibusdam totam ratione aliquam molestiae a tempore."/>
+   <Demo name = "Akash" desc= "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique doloremque voluptatem, fuga molestias iusto eveniet unde vitae ullam mollitia consectetur!" />
+   <Demo name = "Shamim" desc= "  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores, officia" />
+   <Demo name = "Milon" desc= "  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusantium quibusdam mollitia blanditiis ut veniam maxime necessitatibus officiis, accusamus quaerat incidunt aspernatur nihil rem iusto totam, hic repellendus laborum, praesentium neque" />
+   <Demo name = "Shakib" desc= "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi nisi, rem modi impedit porro esse voluptate sapiente suscipit quisquam ducimus, perspiciatis maiores, cum nam ratione reiciendis officiis vel obcaecati saepe enim. Officiis delectus ullam amet natus sequi, veritatis itaque error dolorem excepturi? Sequi, ut nulla." />
 
 
+  
     </>
   )
 }
---
+++ src/components/CardComponent/Demo/Demo.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Demo = ({name, desc}) => {
+  return (
+    <>
+    
+    <h1>{name}</h1>
+    <p>{desc}</p>
+    </>
+  )
+}
+
+export default Demo
